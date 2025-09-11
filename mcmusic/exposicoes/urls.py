19b1fce581a537604e5b1e5bce5e51c2ddb7eb7e
dudumilan <carloseduardomilann@gmail.com>
2025-09-11 18:55:16 -0300

    .

--- mcmusic/exposicoes/urls.py
+++ mcmusic/exposicoes/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from exposicoes import views
+from mcmusic.exposicoes import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='home'),
     path('exposicoes/', views.exposicoes, name='exposicoes'),
     path('linha-do-tempo/', views.linha_do_tempo, name='linha_do_tempo'),
--- mcmusic/settings.py
+++ mcmusic/settings.py
@@ -40,9 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mcmusic.exposicoes.apps.WebsiteConfig',
+    'mcmusic.exposicoes.apps.ExposicoesConfig', 
     'mcmusic.usuarios.apps.UsuariosConfig',
-    'exposicoes',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'mcmusic/static'),)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
